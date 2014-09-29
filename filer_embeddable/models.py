f110609b221f8171fc08720ad2fb69fbec48c657
sthzg <sthzg@gmx.net>
2014-09-29 11:11:23 +0200

    Changed format of .embeddable files from YAML to JSON.

--- filer_embeddable/models.py
+++ filer_embeddable/models.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+import json
 import os
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from filer.models.filemodels import File
-from yaml import load as yaml_load
-try:
-    from yaml import CLoader as Loader, CDumper as Dumper
-except ImportError:
-    from yaml import Loader, Dumper
 
 
 class Embeddable(File):
+    """
+    Base model for more specific embeddable instances.
+    """
     class Meta:
         app_label = 'filer_embeddable'
         verbose_name = _('embeddable')
         verbose_name_plural = _('embeddables')
 
+    #: Src url for embed code. Usually used in <iframe /> tag.
     src = models.CharField(
         _('src'),
         max_length=250,
@@ -24,12 +24,16 @@ class Embeddable(File):
         null=True,
         default=None)
 
+    #: Width value for embedding. (often times this is altered through
+    #: responsive embedding techniques directly from within the templates.
     width = models.PositiveIntegerField(
         _('width'),
         blank=True,
         null=True,
         default=720)
 
+    #: Height value for embedding. (often times this is altered through
+    #: responsive embedding techniques directly from within the templates.
     height = models.PositiveIntegerField(
         _('height'),
         blank=True,
@@ -37,22 +41,46 @@ class Embeddable(File):
         default=405)
 
     def save(self, *args, **kwargs):
-        data = yaml_load(self.file)
+        """
+        Extracts json data from file object to mode fields and updates file
+        object if data is altered in admin.
 
-        self.width = data.get('width', 720)
-        self.height = data.get('height', 720)
-        self.src = data.get('src')
+        Filename extension: *.embeddable
+        File format JSON.
+        """
+        data = json.loads(self.file.file.read())
+
+        if not self.pk:
+            # On upload initially read all data form the json file.
+            self.width = data.get('width', 720)
+            self.height = data.get('height', 405)
+            self.src = data.get('src')
+
+        else:
+            # On change, write changed data to the json file on disk.
+            data['width'] = self.width
+            data['height'] = self.height
+            data['src'] = self.src
+
+            with open(self.file.path, 'w') as f:
+                f.write(json.dumps(data))
 
         super(Embeddable, self).save(*args, **kwargs)
 
     @classmethod
-    def matches_file_type(self, iname, ifile, request):
+    def matches_file_type(cls, iname, ifile, request):
         filename_extensions = ['.embeddable']
         ext = os.path.splitext(iname)[1].lower()
         return ext in filename_extensions
 
 
 class Youtube(Embeddable):
+    """
+    Embeddable model for Youtube video embeds.
+
+    Filename extension: .youtube
+    File format JSON.
+    """
     class Meta:
         app_label = 'filer_embeddable'
         verbose_name = _('youtube embeddable')
