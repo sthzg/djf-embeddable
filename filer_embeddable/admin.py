9aa78b267533297c49a7edbef8327bc66ccae3ad
sthzg <sthzg@gmx.net>
2014-09-29 11:11:58 +0200

    Enabled additional src, width, height fields on admin model forms.

--- filer_embeddable/admin.py
+++ filer_embeddable/admin.py
@@ -1,8 +1,40 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from django import forms
 from django.contrib import admin
 from filer.admin.fileadmin import FileAdmin
 from .models import Embeddable, Youtube
 
-admin.site.register(Embeddable, FileAdmin)
-admin.site.register(Youtube, FileAdmin)
+
+#                                                                          _____
+#                                                                EmbeddableAdmin
+class EmbeddableAdminForm(forms.ModelForm):
+    class Meta:
+        model = Embeddable
+
+
+class EmbeddableAdmin(FileAdmin):
+    form = EmbeddableAdminForm
+
+
+#                                                                          _____
+#                                                                   YoutubeAdmin
+class YoutubeAdminForm(forms.ModelForm):
+    class Meta:
+        model = Youtube
+
+
+class YoutubeAdmin(EmbeddableAdmin):
+    form = YoutubeAdminForm
+
+
+#                                                                          _____
+#                                                                      Fieldsets
+EmbeddableAdmin.fieldsets = EmbeddableAdmin.build_fieldsets(
+    extra_main_fields=('src', 'width', 'height',))
+
+
+#                                                                          _____
+#                                                                   Registration
+admin.site.register(Embeddable, EmbeddableAdmin)
+admin.site.register(Youtube, YoutubeAdmin)
